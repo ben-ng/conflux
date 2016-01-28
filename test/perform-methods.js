4e777b01dffd5d370cd13745ea9c827e2b3afb17
Ben Ng <me@benng.me>
2016-01-28 10:49:27 -0800

    Fix the callback API and add tests

--- test/perform-methods.js
+++ test/perform-methods.js
@@ -31,7 +31,7 @@ tap.test('fails when an unknown method is called', function (t) {
   })
 })
 
-tap.test('can perform methods', function (t) {
+tap.test('can perform methods (promise API)', function (t) {
   var opts = {
       clusterSize: 2
     , channel: {name: 'memory'}
@@ -47,6 +47,9 @@ tap.test('can perform methods', function (t) {
           , a: a
           , b: b
           }
+        }
+      , fez: function () {
+          return null
         }
       , bork: function () {
           return new Error('nope')
@@ -73,27 +76,36 @@ tap.test('can perform methods', function (t) {
   , unsubscribe
   , lastStateOfA
 
-  t.plan(5)
+  t.plan(7)
 
   unsubscribe = a.subscribe(function () {
     lastStateOfA = a.getState()
   })
 
   a.perform('foo', [], 5000)
-  .then(function () {
+  .then(function (act) {
     t.pass('should perform foo')
 
-    return b.perform('bar', ['fee', 'fi'], 5000)
-  })
-  a.perform('bork', [], 5000)
-  .catch(function (err) {
-    t.equals(err.message, 'nope', 'should fail with error from method')
+    t.deepEquals(act, {type: 'foo'}, 'should respond with the foo action')
 
-    return Promise.resolve()
+    return b.perform('bar', ['fee', 'fi'], 5000)
   })
   .then(function () {
     t.pass('should perform bar')
 
+    return a.perform('fez', [], 5000)
+  })
+  .then(function () {
+    t.pass('should noop when Methods return null')
+
+    return a.perform('bork', [], 5000)
+  })
+  .catch(function (err) {
+    t.equals(err.message, 'nope', 'should fail when Methods return Errors')
+
+    return Promise.resolve()
+  })
+  .then(function () {
     async.whilst(function () {
       return a.getState().length !== 2
     }, function (next) {
@@ -109,6 +121,93 @@ tap.test('can perform methods', function (t) {
         unsubscribe()
 
         t.pass('cleanly closed the instances')
+      })
+    })
+  })
+})
+
+tap.test('can perform methods (callback API)', function (t) {
+  var opts = {
+      clusterSize: 2
+    , channel: {name: 'memory'}
+    , methods: {
+        foo: function () {
+          return {
+            type: 'foo'
+          }
+        }
+      , bar: function (a, b) {
+          return {
+            type: 'bar'
+          , a: a
+          , b: b
+          }
+        }
+      , fez: function () {
+          return null
+        }
+      , bork: function () {
+          return new Error('nope')
+        }
+      }
+    , reduce: function (state, action) {
+        if (state == null) {
+          state = []
+        }
+        else {
+          state = JSON.parse(JSON.stringify(state))
+        }
+
+        if (action == null) {
+          return state
+        }
+        else {
+          return state.concat(action)
+        }
+      }
+    }
+  , a = conflux(_.assign({id: uuid.v4()}, opts))
+  , b = conflux(_.assign({id: uuid.v4()}, opts))
+  , unsubscribe
+  , lastStateOfA
+
+  t.plan(7)
+
+  unsubscribe = a.subscribe(function () {
+    lastStateOfA = a.getState()
+  })
+
+  a.perform('foo', [], function (err, act) {
+    t.ifError(err, 'should perform foo without errors')
+    t.deepEquals(act, {type: 'foo'}, 'should respond with the foo action')
+
+    b.perform('bar', ['fee', 'fi'], 5000, function (err) {
+      t.ifError(err, 'should perform bar without errors')
+
+      a.perform('fez', [], 5000, function (err) {
+        t.ifError(err, 'should noop when Methods return null without error')
+
+        a.perform('bork', [], 5000, function (err) {
+          t.equals(err.message, 'nope', 'should fail when Methods return Errors')
+
+          async.whilst(function () {
+            return a.getState().length !== 2
+          }, function (next) {
+            setTimeout(next, 100)
+          }, function () {
+            t.deepEquals(lastStateOfA, [
+              {type: 'foo'}
+            , {type: 'bar', a: 'fee', b: 'fi'}
+            ], 'the state should be correct')
+
+            Promise.all([a.close(), b.close()])
+            .then(function () {
+              unsubscribe()
+
+              t.pass('cleanly closed the instances')
+            })
+          })
+        })
       })
     })
   })
